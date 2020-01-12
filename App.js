34f5f415fec475d40b792e0b1d4ea19fded614ed
Luiz Fernando <fernandoaff1@gmail.com>
2020-01-12 07:57:07 -0300

    Base Code

---
+++ App.js
@@ -0,0 +1,41 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+import {
+  Header,
+  LearnMoreLinks,
+  Colors,
+  DebugInstructions,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+class App extends React.Component{
+  render(){
+    return (
+      <view>
+        
+      </view>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  
+});
+
+export default App;
